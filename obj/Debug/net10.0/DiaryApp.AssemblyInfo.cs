6ec3797b8534d69a981ad8d717adb1b45a77ce65
Ntuthuzelo1 <gwintantuthuzelo@gmail.com>
2026-09-18 02:03:53 +0200

    Implementation of features and templates

--- obj/Debug/net10.0/DiaryApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/DiaryApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiaryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1809f817da8fd218def18bcff41ba1effbb6ace3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f075b90cb58d4f65ed0b9448ce01a5deb493f7dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiaryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiaryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
